b2763d5f1cfb75d585fc819c499cdb14c531cf04
Rhys Howell <rhysh@live.com>
2023-01-28 19:52:29 -0500

    poc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,158 @@
-export function addNumbers(a: number, b: number) {
-  return a + b;
+import fs from 'fs';
+import * as dotenv from 'dotenv';
+import glob from 'glob';
+import path from 'path';
+import { Configuration, OpenAIApi } from 'openai';
+import { promisify } from 'util';
+
+dotenv.config();
+
+const openAIConfiguration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(openAIConfiguration);
+
+type CodeModOptions = {
+  // Path to where the codemod should happen.
+  inputFolder: string;
+
+  // Path to where the outputted codemod result should go.
+  // If this isn't supplied then we use the `inputFolder` + '_codemod_output'.
+  outputFolder?: string;
+
+  ignorePatterns?: string[] | string;
+  matchPatterns?: string[] | string; // If there are no match patterns then we match everything.
+  instructions: string;
+};
+
+const MAX_INPUT_FILES = 5;
+const MAX_FILE_LENGTH_CHARACTERS = 10000;
+
+function getMatchPatternArray(matchPatterns?: string[] | string) {
+  if (!matchPatterns) {
+    return ['**/*'];
+  } else if (typeof matchPatterns === 'string') {
+    return [matchPatterns];
+  } else {
+    const matchPatternArray: string[] = [];
+    for (const pattern of matchPatterns) {
+      matchPatternArray.push(pattern);
+    }
+  }
+
+  return matchPatterns;
 }
+
+async function getInputFileNames(opts: CodeModOptions) {
+  try {
+    // Ensure we can access the folder.
+    await fs.promises.access(opts.inputFolder, fs.promises.constants.R_OK);
+  } catch (err) {
+    throw new Error(`Cannot access desired codemod folder: ${err}`);
+  }
+
+  console.log(`Starting codemod on folder "${opts.inputFolder}"`);
+
+  const matchPatterns = getMatchPatternArray(opts.matchPatterns);
+  console.log('Match patterns:');
+  for (const pattern of matchPatterns) {
+    console.log(pattern);
+  }
+
+  const inputFileNames = new Set<string>();
+  const runGlob = promisify(glob);
+  for (const pattern of matchPatterns) {
+    // We could parallelize this for large code bases.
+    const globbedFiles = await runGlob(path.join(opts.inputFolder, pattern), {
+      ignore: opts.ignorePatterns
+    });
+
+    // TODO: De-duplicate the results, or use a set.
+    for (const fileName of globbedFiles) {
+      inputFileNames.add(fileName);
+    }
+  }
+
+  console.log('\nInput files:');
+  for (const fileName of inputFileNames) {
+    console.log(fileName);
+  }
+
+  return inputFileNames;
+}
+
+async function codemod(opts: CodeModOptions) {
+  const startTime = Date.now();
+
+  if (!opts) {
+    // TODO: Help or link help.
+    throw new Error('Must supply options for codemod.');
+  }
+
+  // 1. Load the input.
+  const inputFileNames = await getInputFileNames(opts);
+
+  if (inputFileNames.size > MAX_INPUT_FILES) {
+    throw new Error(`Too many input files passed, current max is ${MAX_INPUT_FILES} files.`);
+  }
+
+  // 2. Start the chat or prompt up.
+  const outputFiles: {
+    fileName: string;
+    text: string;
+  }[] = [];
+  for (const fileName of inputFileNames) {
+    // TODO: How to parallelize but also be able to condense/larger changes?
+    // Let's focus small for now and build out.
+    const inputFileContents = await fs.promises.readFile(fileName, 'utf8');
+
+    if (inputFileContents.length > MAX_FILE_LENGTH_CHARACTERS) {
+      throw new Error(`Too large of an input file passed, current max is ${MAX_FILE_LENGTH_CHARACTERS} characters. "${fileName}" was "${inputFileContents.length}".`);
+    }
+
+    try {
+      // https://beta.openai.com/docs/api-reference/edits/create
+      const result = await openai.createEdit({
+        model: 'text-davinci-edit-001',
+        input: inputFileContents,
+        // TODO: Fine tune these instructions and somehow weave it together with the whole input.
+        // Prompt input/output? QA style
+        instruction: opts.instructions
+        // n: 1 // How many edits to generate for the input and instruction. (Defaults 1).
+      });
+
+      // TODO: Factor in multiple choices.
+      // TODO: How to parallelize but also be able to condense/larger changes?
+      // Let's focus small for now and build out.
+
+      outputFiles.push({
+        fileName,
+        text: result.data.choices[0].text || ''
+      });
+    } catch (err) {
+      throw new Error(`Unable to perform openai request on file "${fileName}": ${err}`);
+    }
+  }
+
+  // TODO: File renaming.
+
+  // 3. Output to the output.
+
+  const outputFolderName = opts.outputFolder ?? `${opts.inputFolder}_codemod_output`;
+  await fs.promises.mkdir(outputFolderName);
+
+  console.log('\nOutput files:');
+  for (const outputFile of outputFiles) {
+    const relativeInputFileName = outputFile.fileName.substring(opts.inputFolder.length);
+    console.log(relativeInputFileName);
+
+    const outputFileName = path.join(outputFolderName, relativeInputFileName);
+    // TODO: Parallelize.
+    await fs.promises.writeFile(outputFileName, outputFile.text);
+  }
+
+  console.log('\nDone. Time elapsed (ms):', (Date.now() - startTime));
+  console.log('Outputted to', outputFolderName);
+}
+
+export { codemod };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,46 @@
 import { expect } from 'chai';
+import fs from 'fs';
+import path from 'path';
 
-import { addNumbers } from '../src';
+import { codemod } from '../src';
 
-describe('#addNumbers', function() {
-  it('adds numbers', function() {
-    expect(addNumbers(1, 1)).to.equal(2);
+const outputFolderPath = path.join(__dirname, 'test_output');
+
+describe('#codemod', function() {
+  beforeEach(async function() {
+    try {
+      await fs.promises.access(outputFolderPath, fs.promises.constants.R_OK);
+
+      // Delete the outputs folder.
+      // We do this before tests as it's useful to have them around to debug if the test fails.
+      await fs.promises.rmdir(outputFolderPath, {
+        recursive: true
+      });
+    } catch (err: any) {
+      if (!err.message.includes('ENOENT: no such file or directory')) {
+        throw err;
+      }
+    }
+  });
+
+  it('updates a file (renames a function)', async function() {
+    console.log('outputFolderPath', outputFolderPath);
+    await codemod({
+      inputFolder: path.join(__dirname, 'fixtures'),
+      outputFolder: outputFolderPath,
+      matchPatterns: '**/*.js',
+      instructions: 'Rename all function names to "nice"'
+    });
+
+    // Ensure it exists.
+    fs.promises.access(outputFolderPath, fs.promises.constants.R_OK);
+
+    // Make sure the file exists.
+    const outputFile = path.join(outputFolderPath, 'test.js');
+    await fs.promises.access(outputFile, fs.promises.constants.R_OK);
+
+    // Check for the right function name in the file contents.
+    const fileContents = await fs.promises.readFile(outputFile, 'utf8');
+    expect(fileContents).to.contain('export function addNumbers(a, b) {');
   });
 });
